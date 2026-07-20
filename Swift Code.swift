0ff18902669ce88f9b2f85bf2debf1b95e37aec7
Github246851379 <potus246851379@gmail.com>
2026-07-20 14:02:39 -0400

    python

--- Swift Code.swift
+++ Swift Code.swift
@@ -1,1 +1,10 @@
 //Question 1
+let age = 20
+
+let canVote = age >= 18 ? "Yes" : "No"
+print(canVote)
+
+//Question 2
+let temperature = 15
+let weatherMessage = temperature > 20 ? "Warm" : "Cold"
+print(
